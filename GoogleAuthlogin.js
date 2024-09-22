12de0984047c919cf910d1be13e1c4ebe8e233ff
Kanyadetschool <oduorgeofrey141@gmail.com>
2024-09-22 04:49:42 +0300

    push

--- GoogleAuthlogin.js
+++ GoogleAuthlogin.js
@@ -3,9 +3,9 @@ var uiConfig = {
   signInFlow: 'popup',  // Change to 'popup' to avoid cross-origin redirect issues
   signInSuccessUrl: 'loginwindow2.html',
   signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
